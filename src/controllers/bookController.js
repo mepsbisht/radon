f2182ca28df4c9d71ecdf3a7577b5d4430c9928a
mepsbisht <prathamsinghbisht@gmail.com>
2022-06-08 15:22:34 +0530

    Assignment Done session-1-mongo-3-more(2)

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,5 +1,5 @@
 const { count } = require("console");
-const BookModel = require("../models/bookModel");
+// const BookModel = require("../models/bookModel");
 const AuthorModel = require("../models/authorModel");
 const bookModel = require("../models/bookModel");
 
@@ -45,8 +45,46 @@ const bookFind = async function (req, res) {
   res.send({ msg: arr });
 };
 
+const bookByAuthorId = async function (req, res) {
+  let result1 = await AuthorModel.find({ author_id: req.params.authorId });
+  let arr = [];
+  for (i = 0; i < result1.length; i++) {
+    let bookName = await bookModel
+      .find({
+        author_id: result1[i].author_id,
+      })
+      .select({ name: 1, _id: 0 });
+    arr.push(bookName);
+  }
+  res.send({ msg: arr });
+};
+
+const oldAuthor = async function (req, res) {
+  let result1 = await AuthorModel.find({ age: { $gt: 50 } });
+  // res.send(result1);
+  let arr = [];
+  for (i = 0; i < result1.length; i++) {
+    let bookName = await bookModel.find({
+      author_id: result1[i].author_id,
+      ratings: {$gt: 4}
+    }).select({author_id:1,_id:0})
+
+    // console.log(bookName);
+    if (bookName.length > 0) {
+      for (k = 0; k < bookName.length; k++) {
+        let authorData = await AuthorModel.find({ author_id: bookName[i]['author_id'] }).select({author_name:1, age:1, _id:0});
+
+        arr.push(authorData);
+      }
+    }
+    
+  }
+  res.send({msg:arr});
+};
 module.exports.createBook = createBook;
 module.exports.createAuthor = createAuthor;
 module.exports.bookList = bookList;
 module.exports.priceUpdate = priceUpdate;
 module.exports.bookFind = bookFind;
+module.exports.bookByAuthorId = bookByAuthorId;
+module.exports.oldAuthor = oldAuthor;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
 // const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const BookController = require("../controllers/bookController");
 
-router.post("/createBook", BookController.createBook)
-router.post("/createAuthor", BookController.createAuthor)
-router.get("/bookList", BookController.bookList)
-router.get("/priceUpdate", BookController.priceUpdate)
-router.get("/bookFind", BookController.bookFind)
+router.post("/createBook", BookController.createBook);
+router.post("/createAuthor", BookController.createAuthor);
+router.get("/bookList", BookController.bookList);
+router.get("/priceUpdate", BookController.priceUpdate);
+router.get("/bookFind", BookController.bookFind);
+router.get("/books/:authorId" ,BookController.bookByAuthorId);
+router.get("/oldAuthor" ,BookController.oldAuthor);
 
 module.exports = router;
