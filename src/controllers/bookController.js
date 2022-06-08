53ef833b032c41140ba265007d1866a3209efcb8
mepsbisht <prathamsinghbisht@gmail.com>
2022-06-08 23:06:23 +0530

    Assignment Done more on mongo

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -15,36 +15,6 @@ const createAuthor = async function (req, res) {
   res.send({ msg: savedAuthor });
 };
 
-const bookList = async function (req, res) {
-  let result1 = await AuthorModel.find({ author_name: "Chetan Bhagat" });
-  let id = result1[0].author_id;
-  let result2 = await bookModel.find({ author_id: id });
-  res.send({ msg: result2 });
-};
-
-const priceUpdate = async function (req, res) {
-  let data = await bookModel.findOneAndUpdate(
-    { name: "Two states" },
-    { $set: { price: 100 } },
-    { new: true }
-  );
-  let result1 = await AuthorModel.find({ author_id: data.author_id });
-  let price = data.price;
-  res.send({ author_name: result1[0].author_name, price });
-};
-
-const bookFind = async function (req, res) {
-  let bookRange = await bookModel.find({ price: { $lte: 50, $gte: 100 } });
-  let arr = [];
-  for (let i = 0; i < bookRange.length; i++) {
-    let authorName = await AuthorModel.find({
-      author_id: bookRange[i].author_id,
-    }).select({ author_name: 1, author_id: 1, _id: 0 });
-    arr.push(authorName);
-  }
-  res.send({ msg: arr });
-};
-
 const bookByAuthorId = async function (req, res) {
   let result1 = await AuthorModel.find({ author_id: req.params.authorId });
   let arr = [];
@@ -64,27 +34,28 @@ const oldAuthor = async function (req, res) {
   // res.send(result1);
   let arr = [];
   for (i = 0; i < result1.length; i++) {
-    let bookName = await bookModel.find({
-      author_id: result1[i].author_id,
-      ratings: {$gt: 4}
-    }).select({author_id:1,_id:0})
+    let bookName = await bookModel
+      .find({
+        author_id: result1[i].author_id,
+        ratings: { $gt: 4 },
+      })
+      // .select({ author_id: 1, _id: 0 });
 
     // console.log(bookName);
     if (bookName.length > 0) {
       for (k = 0; k < bookName.length; k++) {
-        let authorData = await AuthorModel.find({ author_id: bookName[i]['author_id'] }).select({author_name:1, age:1, _id:0});
+        let authorData = await AuthorModel.find({
+          author_id: bookName[i].author_id,
+        }).select({ author_name: 1, age: 1, _id: 0 });
 
         arr.push(authorData);
       }
     }
-    
   }
-  res.send({msg:arr});
+  res.send({ msg: arr });
 };
 module.exports.createBook = createBook;
 module.exports.createAuthor = createAuthor;
-module.exports.bookList = bookList;
-module.exports.priceUpdate = priceUpdate;
-module.exports.bookFind = bookFind;
+
 module.exports.bookByAuthorId = bookByAuthorId;
 module.exports.oldAuthor = oldAuthor;
--- src/routes/route.js
+++ src/routes/route.js
@@ -6,9 +6,6 @@ const BookController = require("../controllers/bookController");
 
 router.post("/createBook", BookController.createBook);
 router.post("/createAuthor", BookController.createAuthor);
-router.get("/bookList", BookController.bookList);
-router.get("/priceUpdate", BookController.priceUpdate);
-router.get("/bookFind", BookController.bookFind);
 router.get("/books/:authorId" ,BookController.bookByAuthorId);
 router.get("/oldAuthor" ,BookController.oldAuthor);
 
