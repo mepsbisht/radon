8722a69ac366f65af2008463dd5c412f332d9dc5
mepsbisht <prathamsinghbisht@gmail.com>
2022-06-14 20:20:20 +0530

    Assignment done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,14 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-const createUser = async function (abcd, xyz) {
+const createUser = async function (req, res) {
   //You can name the req, res objects anything.
-  //but the first parameter is always the request 
+  //but the first parameter is always the request
   //the second parameter is always the response
-  let data = abcd.body;
+  let data = req.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
+  // console.log(req.newAtribute);
+  res.send({ msg: savedData });
 };
 
 const loginUser = async function (req, res) {
@@ -41,23 +41,6 @@ const loginUser = async function (req, res) {
 };
 
 const getUserData = async function (req, res) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
-
-  //If no token is present in the request header return error
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-
-  console.log(token);
-  
-  // If a token is present then decode the token with verify function
-  // verify takes two inputs:
-  // Input 1 is the token to be decoded
-  // Input 2 is the same secret with which the token was generated
-  // Check the value of the decoded token yourself
-  let decodedToken = jwt.verify(token, "functionup-radon");
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
-
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
   if (!userDetails)
@@ -67,10 +50,18 @@ const getUserData = async function (req, res) {
 };
 
 const updateUser = async function (req, res) {
-// Do the same steps here:
-// Check if the token is present
-// Check if the token present is a valid token
-// Return a different error message in both these cases
+  // Do the same steps here:
+  // Check if the token is present
+  // Check if the token present is a valid token
+  // Return a different error message in both these cases
+
+  // let token = req.headers["x-auth-token"];
+  // if (!token)
+  //   return res.send({ status: "false", msg: "token must be present" });
+
+  // let decodedToken = jwt.verify(token, "functionup-radon");
+  // if (!decodedToken)
+  //   return res.send({ status: "false", msg: "token is in invalid" });
 
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
@@ -80,11 +71,29 @@ const updateUser = async function (req, res) {
   }
 
   let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+  let updatedUser = await userModel.findOneAndUpdate(
+    { _id: userId },
+    userData,
+    { upsert: true }
+  );
   res.send({ status: updatedUser, data: updatedUser });
 };
 
+const deleteData = async function (req, res) {
+  let userId = req.params.userId;
+  let user = await userModel.findById(userId);
+  if (!user) return res.send({ status: "false", msg: "user is not valid" });
+
+  let userData = req.body;
+  let updatedUser = await userModel.findOneAndUpdate(
+    { _id: userId },
+    { $set: { isDeleted: true } },
+    { upsert: true }
+  );
+  res.send({ status: true, data: updatedUser });
+};
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deleteData = deleteData;
---
+++ src/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const mid1 = async function (req, res, next) {
+  let token = req.headers["x-Auth-token"];
+  if (!token) token = req.headers["x-auth-token"];
+
+  //If no token is present in the request header return error
+  if (!token) return res.send({ status: false, msg: "token must be present" });
+
+  let decodedToken = jwt.verify(token, "functionup-radon");
+  if (!decodedToken)
+    return res.send({ status: false, msg: "token is invalid" });
+  next();
+};
+
+module.exports.mid1 = mid1;
