69bde20e750793dba7653147cd514db88875fe03
mepsbisht <prathamsinghbisht@gmail.com>
2022-06-15 12:17:53 +0530

    Assignment done auth1(authorisation)

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
 
+// AUTHENTICATION OF USER BY USER DETAILS
+
 const mid1 = async function (req, res, next) {
   let token = req.headers["x-Auth-token"];
   if (!token) token = req.headers["x-auth-token"];
@@ -10,6 +12,17 @@ const mid1 = async function (req, res, next) {
   let decodedToken = jwt.verify(token, "functionup-radon");
   if (!decodedToken)
     return res.send({ status: false, msg: "token is invalid" });
+
+  // AUTHORISATION FOR USER TO MAKE CHANGES 
+  
+  let userToBeModified = req.params.userId;
+  let userLoggedIn = decodedToken.userId;
+
+  if (userToBeModified != userLoggedIn)
+    return res.send({
+      status: "false",
+      msg: "you are not authorised to update anothers data",
+    });
   next();
 };
 
